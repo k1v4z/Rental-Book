bc2e07860f3f357613e4b33a80f88a8b369db7d5
k1v4z <addnguyen32gmail.com>
2024-03-12 17:20:04 +0700

    add api add,delete book

--- src/controller/ApiController.js
+++ src/controller/ApiController.js
@@ -1,4 +1,5 @@
-const { signUp, login, getIdUser } = require("../service/CRUD.Service");
+const { signUp, login, getIdUser, increaseQuantityBook,
+    decreaseQuantityBook, deleteBook } = require("../service/CRUD.Service");
 
 const apisignup = async (req, res) => {
     const { status, message } = await signUp(req);
@@ -21,7 +22,6 @@ const apilogin = async (req, res) => {
 const apigetIdUser = async (req, res) => {
     const { id } = await getIdUser(req);
 
-
     return res.status(200).json({
         id: id,
     })
@@ -35,6 +35,54 @@ const apiCheckPermission = (req, res) => {
     })
 }
 
+const apiIncreaseQuantity = async (req, res) => {
+    const bookId = req.query.id;
+
+    const bookExist = await increaseQuantityBook(bookId);
+
+    if (bookExist) {
+        return res.status(200).json({
+            message: 'increase quantity of book successful'
+        })
+    }
+
+    return res.status(400).json({
+        message: `This book not existed`
+    })
+}
+
+const apiDecreaseQuantity = async (req, res) => {
+    const bookId = req.query.id;
+
+    const status = await decreaseQuantityBook(bookId);
+
+    if (status.check) {
+        return res.status(200).json({
+            message: status.message
+        })
+    }
+
+    return res.status(400).json({
+        message: status.message
+    })
+}
+
+const apiDeleteBook = async (req, res) => {
+    const bookId = req.query.id;
+
+    const deleteB = await deleteBook(bookId); //deleteB = deleteBook, it's duplicate
+    if (deleteB) {
+        return res.status(200).json({
+            message: 'Delete Successful'
+        });
+    }
+
+    return res.status(400).json({
+        message: 'Book not exist'
+    })
+}
+
 module.exports = {
-    apisignup, apilogin, apigetIdUser, apiCheckPermission
+    apisignup, apilogin, apigetIdUser, apiCheckPermission,
+    apiIncreaseQuantity, apiDecreaseQuantity, apiDeleteBook
 }
