e4a6c217c190873b05db82b47ad20f575fdd83a3
k1v4z <addnguyen32gmail.com>
2024-03-12 17:25:16 +0700

    change status

--- src/middleware/valid.middleware.js
+++ src/middleware/valid.middleware.js
@@ -5,21 +5,33 @@ const checkValid = (req, res, next) => {
 
     //check username, password is valid
     if (username.length < 6) {
-        return res.status(409).json({
-            status: 409,
+        return res.status(400).json({
+            status: 400,
             message: 'username must have more than 6 characters'
         });
     }
 
     if (passwords.length < 6) {
-        return res.status(409).json({
-            status: 409,
+        return res.status(400).json({
+            status: 400,
             message: 'password must have more than 6 characters'
         });
     }
     next();
 }
 
+const checktypeOfQueryId = (req, res, next) => {
+    const bookId = req.query.id;
+    //check id book
+    //If id book is a letter send a message else go to controller
+    if (bookId.match(/[a-z]/i)) {
+        return res.status(400).json({
+            message: 'bookId is not valid'
+        })
+    }
+    next();
+}
+
 module.exports = {
-    checkValid
+    checkValid, checktypeOfQueryId
 }
